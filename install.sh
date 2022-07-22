d9ccf8dce297fc539c748b7e6578563ad2e3e516
Hayato Mizuno <pocotan001@gmail.com>
2022-07-22 23:23:19 +0900

    updates

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 for f in ~/Dropbox/Development/dotfiles/.[^.]*; do
   [[ $f =~ /.git$ ]] && continue
   [[ $f =~ /.DS_Store$ ]] && continue
-  [[ $f =~ /.idea$ ]] && continue
 
   ln -snfv "$f" ~
 done
