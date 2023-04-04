b602f6d54d7aa157b77fe26224141539c2ba4627
Hayato Mizuno <pocotan001@gmail.com>
2023-04-04 14:40:29 +0900

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,7 @@ starship init fish | source
 # asdf
 # https://asdf-vm.com/
 echo -e "\nsource "(brew --prefix asdf)"/libexec/asdf.fish" >> ~/.config/fish/config.fish
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # iTerm2 shell integration
 # https://iterm2.com/documentation-shell-integration.html
